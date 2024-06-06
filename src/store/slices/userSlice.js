b897bc0f8174c4758f01773e27ae3b460c51ea89
Carlos Gámez <hardpx@gmail.com>
2024-06-06 11:58:39 -0600

    add extra reducers watching for the action types made by the thunk

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
 
 const initialState = {
   data: [],
@@ -10,6 +11,19 @@ const userSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
 });
 
 export const usersReducer = userSlice.reducer;
