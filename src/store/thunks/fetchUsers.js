5bf7bfa0d2bb49b5c4e20a88d999d63018f752fe
Carlos Gámez <hardpx@gmail.com>
2024-06-07 12:25:58 -0600

    dispatch thunk

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
-  const res = await axios.get('http://localhost3000/users');
+  const res = await axios.get('http://localhost:3000/users');
 
   return res.data;
 });
