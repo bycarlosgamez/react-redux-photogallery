d2475fe5e99e83942bd1d4503dbc4254e1611dbd
Carlos Gámez <hardpx@gmail.com>
2024-06-28 12:20:22 -0600

    redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+import UsersList from './components/UsersList';
+
+function App() {
+  return (
+    <div className='container mx-auto'>
+      <UsersList />
+    </div>
+  );
 }
+
+export default App;
