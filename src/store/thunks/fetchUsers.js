b2cc9fba2f08bfa29bbf42e03b42e3cd9ad07382
Carlos Gámez <hardpx@gmail.com>
2024-07-06 17:36:02 -0600

    added skeleto loading component

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,17 @@ import axios from 'axios';
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const res = await axios.get('http://localhost:3000/users');
 
+  // DEV TEST
+  await pause(3000);
+
   return res.data;
 });
+
+// DEV TEST
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
